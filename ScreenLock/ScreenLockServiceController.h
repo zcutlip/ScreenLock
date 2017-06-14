e3084aea3bc5bbf0dd41e7e6d88ab5d9470d2f9a
Zachary Cutlip <uid000@gmail.com>
2017-06-14 08:19:41 -0700

    removed ViewController, added controller for NSService

---
+++ ScreenLock/ScreenLockServiceController.h
@@ -0,0 +1,13 @@
+//
+//  ScreenLockServiceController.h
+//  ScreenLock
+//
+//  Created by Zachary Cutlip on 6/13/17.
+//  Copyright © 2017 Zachary Cutlip. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ScreenLockServiceController : NSObject
+
+@end
---
+++ ScreenLock/ScreenLockServiceController.m
@@ -0,0 +1,13 @@
+//
+//  ScreenLockServiceController.m
+//  ScreenLock
+//
+//  Created by Zachary Cutlip on 6/13/17.
+//  Copyright © 2017 Zachary Cutlip. All rights reserved.
+//
+
+#import "ScreenLockServiceController.h"
+
+@implementation ScreenLockServiceController
+
+@end
